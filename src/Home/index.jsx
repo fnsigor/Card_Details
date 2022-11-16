46e2051610cd3c0f0d4e03163d89d75142430ccf
Igor Fernandes <igorfernamdez@gmail.com>
2022-11-15 22:44:48 -0300

    validation

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -5,6 +5,10 @@ import { BackCard } from '../components/BackCard'
 import { Thanks } from '../components/Thanks'
 import './style.css'
 
+
+import { useForm } from "react-hook-form";
+
+
 export function Home() {
 
     const [cardHolder, setCardHolder] = useState("Jane Appleseed")
@@ -15,30 +19,112 @@ export function Home() {
     const [cvc, setCvc] = useState('000')
     const [detailsStatus, setDetailsStatus] = useState(false)
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        criteriaMode: "all"
+    });
 
-    const handleChange = event => {
-    
+    const holderInput = <input
+        maxLength={23} placeholder="e.g Jane Appleseed"
+        {...register("holder", {
+            onChange: e => setCardHolder(e.target.value),
+            required: "This input is required.",
+            pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Alphabetical characters only"
+            },
+            minLength: {
+                value: 3,
+                message: "This input must exceed 2 characters"
+            },
+        })}
 
-        setCardHolder(event.target.value);
-    };
+    />
 
+    const numberInput = <input
+        maxLength={16} placeholder="e.g 1234 5678 9123 0000"
+        {...register("number", {
+            onChange: e => setCardNumber(e.target.value),
+            required: "This input is required.",
+            pattern: {
+                value: /\d+/,
+                message: "This input is number only."
+            },
+            minLength: {
+                value: 16,
+                message: "This input must have 16 characters"
+            },
+        })} />
 
-    const nameInput = <input type="text" minLength={6} maxLength={24} placeholder="e.g Jane Appleseed" id="cardholder" onChange={handleChange}  />
-    const numberInput = <input type="text" minLength={16} maxLength={16} placeholder="e.g 1234 5678 9123 0000" id="cardnumber" onChange={e => setCardNumber(e.target.value)} />
-    const monthInput = <input type='text' minLength={2} maxLength={2} id='start' aria-labelledby='lblRange' placeholder="MM" onChange={e => setCardMonth(e.target.value)} />
-    const yearInput = <input type='text' minLength={2} maxLength={2} id='end' aria-labelledby='lblRange' placeholder="YY" onChange={e => setCardYear(e.target.value)} />
+    const monthInput = <input
+        maxLength={2} placeholder="MM"
+        {...register("month", {
+            onChange: e => setCardMonth(e.target.value),
+            required: "Month input is required.",
+            pattern: {
+                value: /\d+/,
+                message: "Month input is number only."
+            },
+            minLength: {
+                value: 2,
+                message: "Month input must have 2 characters"
+            },
+        })} />
+    const yearInput = <input
+        maxLength={2} placeholder="YY"
+        {...register("year", {
+            onChange: e => setCardYear(e.target.value),
+            required: "Year input is required.",
+            pattern: {
+                value: /\d+/,
+                message: "Year input is number only."
+            },
+            minLength: {
+                value: 2,
+                message: "Year input must have 2 characters"
+            },
+        })} />
 
-    const cvcInput = <input type="text" minLength={3} maxLength={3} title="please enter number only" placeholder="e.g 123" id="cvc" onChange={e => setCvc(e.target.value)} />
+    const cvcInput = <input
+        maxLength={3} placeholder="e.g 123"
+        {...register("cvc", {
+            onChange: e => setCvc(e.target.value),
+            required: "CVC input is required.",
+            pattern: {
+                value: /\d+/,
+                message: "CVC input is number only."
+            },
+            minLength: {
+                value: 3,
+                message: "CVC input must have 2 characters"
+            },
+        })} />
 
 
     return (
         <>
             <div className="cards-container">
-                <FrontCard name={cardHolder} number={cardNumber} mm={cardMonth} yy={cardYear} />
+                <FrontCard
+                    name={cardHolder}
+                    number={cardNumber}
+                    mm={cardMonth}
+                    yy={cardYear}
+                />
                 <BackCard cvc={cvc} />
             </div>
 
-            <Form nameInput={nameInput} numberInput={numberInput} monthInput={monthInput} yearInput={yearInput} cvcInput={cvcInput} />
+            <Form
+                holderInput={holderInput}
+                numberInput={numberInput}
+                monthInput={monthInput}
+                yearInput={yearInput}
+                cvcInput={cvcInput}
+                submit={handleSubmit}
+                errors={errors}
+            />
 
 
 
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,37 +1,104 @@
 
 import './style.css'
+import { ErrorMessage } from "@hookform/error-message";
 
-
-export function Form({nameInput, numberInput, monthInput, yearInput, cvcInput}) {
+export function Form({ holderInput, numberInput, monthInput, yearInput, cvcInput, submit, errors }) {
 
 
 
 
     return (
-        <form action="">
+        <form onSubmit={submit()}>
             <div className="inputBox">
                 <label htmlFor="cardholder" >Cardholder name</label>
-                {nameInput}
+                {holderInput}
+                <ErrorMessage
+
+                    errors={errors}
+                    name="holder"
+                    render={({ messages }) => {
+                        console.log("messages", messages);
+                        return messages
+                            ? Object.entries(messages).map(([type, message]) => (
+                                <p key={type}>{message}</p>
+                            ))
+                            : null;
+                    }}
+                />
             </div>
 
             <div className="inputBox">
                 <label htmlFor="cardnumber" >Card Number</label>
                 {numberInput}
+                <ErrorMessage
+                    errors={errors}
+                    name="number"
+                    render={({ messages }) => {
+                        console.log("messages", messages);
+                        return messages
+                            ? Object.entries(messages).map(([type, message]) => (
+                                <p key={type}>{message}</p>
+                            ))
+                            : null;
+                    }}
+                />
             </div>
 
-            <div className="inputBox numbers">
-                
-                <div className=" exp">
-                    <label id='lblRange'>EXP. DATE &#40;MM&#47;YY&#41;</label>
-                    <div className="exp-inputs">
-                        {monthInput}
-                        {yearInput}
+            <div className=" numbers">
+
+                <div className="containerInputs">
+
+                    <div className="exp inputBox"  >
+                        <label id='lblRange'>EXP. DATE &#40;MM&#47;YY&#41;</label>
+                        <div className="exp-inputs">
+                            {monthInput}
+                            {yearInput}
+                        </div>
+                    </div>
+
+                    <div className="inputBox cvc">
+                        <label htmlFor="cvc" >CVC</label>
+                        {cvcInput}
                     </div>
                 </div>
 
-                <div className=" cvc">
-                    <label htmlFor="cvc" >CVC</label>
-                   {cvcInput}
+                <div className="containerErrors">
+                    <ErrorMessage
+                        errors={errors}
+                        name="month"
+                        render={({ messages }) => {
+                            console.log("messages", messages);
+                            return messages
+                                ? Object.entries(messages).map(([type, message]) => (
+                                    <p key={type}>{message}</p>
+                                ))
+                                : null;
+                        }}
+                    />
+                    <ErrorMessage
+                        errors={errors}
+                        name="year"
+                        render={({ messages }) => {
+                            console.log("messages", messages);
+                            return messages
+                                ? Object.entries(messages).map(([type, message]) => (
+                                    <p key={type}>{message}</p>
+                                ))
+                                : null;
+                        }}
+                    />
+                    <ErrorMessage
+                        errors={errors}
+                        name="cvc"
+                        render={({ messages }) => {
+                            console.log("messages", messages);
+                            return messages
+                                ? Object.entries(messages).map(([type, message]) => (
+                                    <p key={type}>{message}</p>
+                                ))
+                                : null;
+                        }}
+                    />
                 </div>
             </div>
 
